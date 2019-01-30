55e8daa97381136fe047e6578e268b6e757ac92b
idoco <ido.c.mail@gmail.com>
2019-01-30 17:27:36 +0200

    read system data from csv and load to map on selection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// Map creation
 var mainMap = L.map('mainMap').setView([32.0795, 34.7812], 14);
 
 var tiles = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -6,14 +7,19 @@ var tiles = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?ac
     accessToken: 'pk.eyJ1IjoiaWRvY28iLCJhIjoiY2ptM2JnbW5lMGN6czN2bW14NXUzMGZ2YyJ9.xIvjUlL3cPhak8p0ucOnxg'
 }).addTo(mainMap);
 
+let currentMarkerGroup; // the currently displayed marker layer 
+
+async function fetchWithCors(url) {
+    return await fetch('https://cors.io/?' + url);
+}
+
 async function loadBikes(url) {
-    let response = await fetch(url + '?time=' + Date.now());
+    let response = await fetchWithCors(url + '?time=' + Date.now());
     let freeBikes = await response.json();
     return freeBikes.data.bikes;
 }
 
 async function showBikes(bikes) {
-
     let markers = [];
     for (let i in bikes) {
         let bike = bikes[i];
@@ -26,14 +32,59 @@ async function showBikes(bikes) {
         markers.push(marker);
     }
 
-    var group = L.featureGroup(markers).addTo(mainMap);
-    mainMap.fitBounds(group.getBounds());
-
+    currentMarkerGroup = L.featureGroup(markers).addTo(mainMap);
+    mainMap.fitBounds(currentMarkerGroup.getBounds());
 }
 
-async function main(url) {
+async function loadCsv(url) {
+    let response = await fetch(url);
+    let rawCsv = await response.text()
+    let csv = $.csv.toArrays(rawCsv)
+    csv.shift() // remove header row
+    csv.unshift(["IL", "Bird gbfs", "Tel Aviv", "BIRD_TLV", "bird.co", 'https://mds.bird.co/gbfs']); // Add bird 
+    return csv;
+}
+
+async function main() {
+
+    let csv = await loadCsv('https://raw.githubusercontent.com/NABSA/gbfs/master/systems.csv');
+
+    var selectSystem = document.getElementById("select-system");
+
+    for (let i in csv) {
+        let opt = csv[i][1];
+        let el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        selectSystem.appendChild(el);
+    }
+
+    selectSystem.addEventListener("change", async () => {
+        const selectedIndex = document.getElementById("select-system").selectedIndex;
+        const systemUrl = csv[selectedIndex][5];
+
+        let response = await fetchWithCors(systemUrl + '?time=' + Date.now());
+        let systemData = await response.json();
+
+        if (systemData.data && systemData.data.en.feeds) {
+            const feeds = systemData.data.en.feeds;
+            for (let i in feeds) {
+                if (feeds[i].name == 'free_bike_status' || feeds[i].name == 'free_bikes') {
+                    freeBikesUrl = feeds[i].url;
+                    break;
+                }
+            }
+
+            mainMap.removeLayer(currentMarkerGroup);
+            let bikes = await loadBikes(freeBikesUrl);
+            await showBikes(bikes)
+        }
+
+    });
+
+    // Load bird Tel aviv on launch
     let bikes = await loadBikes('https://mds.bird.co/gbfs/tel-aviv/free_bikes');
-    showBikes(bikes)
+    let displayedMarkers = await showBikes(bikes)
 }
 
 main();
